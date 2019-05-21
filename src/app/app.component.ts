030b87359754e8911fe6bc1f0e26b4f7ae711d5e
NumPoet <bonekob2rt@gmail.com>
2019-05-21 08:35:42 -0500

    reimplementando tutorial de fazt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import { Link } from './link/link.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-links-votes-app';
+
+  links: Link[];
+
+  constructor() {
+    this.links = [
+      new Link('Angular', 'https://angular.oi', 100),
+      new Link('1Google', 'http://google.com', 45),
+      new Link('NumPoet', 'https:numpoet.com', 98),
+    ];
+  }
+
+  addLink(title: HTMLInputElement, link: HTMLInputElement){
+    this.links.push(new Link(title.value, link.value));
+    title.value = '';
+    link.value = '';
+  }
 }
---
+++ src/app/link/link.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Link } from './link.model';
+
+@Component({
+  selector: 'app-link',
+  templateUrl: './link.component.html',
+  styleUrls: ['./link.component.css']
+})
+export class LinkComponent implements OnInit {
+
+  @Input() link: Link;
+
+  constructor() { 
+    
+    
+  }
+
+  ngOnInit() {
+    
+  }
+
+}
